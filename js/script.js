801c388b17be19559c27a39a148cf5ca8c0b89bd
FreuryGC <freurygc@MacBook-Air-de-Freury.local>
2025-01-26 16:33:28 -0700

    2

--- js/script.js
+++ js/script.js
@@ -5,9 +5,24 @@ const spans = document.querySelectorAll('span'); // Selecciona el primer element
 const darkmode_switch = document.querySelector('.switch'); // Selecciona el switch por clase
 const circle = document.querySelector('.circle'); // Selecciona el circulo por clase
 const menu = document.querySelector('.menu'); // Selecciona el menu por clase
+const main = document.querySelector("main");
 
 menu.addEventListener('click', () => {
-
+  sidebar.classList.toggle("max_sidebar");
+  if(sidebar.classList.contains("max_sidebar")){
+    menu.children[0].style.display = "none";
+    menu.children[1].style.display = "block";
+  }else{
+    menu.children[0].style.display = "block";
+    menu.children[1].style.display = "none";
+  }
+  if(window.innerWidth<=320){
+    sidebar.classList.add("mini_sidebar");
+    main.classList.add("min_main");
+    spans.forEach((span)=>{
+      span.classList.add("hide");
+    })
+  }
 });
 
 darkmode_switch.addEventListener('click', () => {
@@ -19,6 +34,7 @@ darkmode_switch.addEventListener('click', () => {
 // Alterna clases al hacer clic en el logo, agrega la clase si no la tiene al dar clic y al dar clic de nuevo la quita 
 logo.addEventListener('click', () => {
   sidebar.classList.toggle('mini_sidebar'); // Contrae/expande la barra lateral
+  main.classList.toggle("min_main");
   spans.forEach((span)=>{
     span.classList.toggle('hide'); // Muestra/Oculta los textos de la barra lateral
   })
